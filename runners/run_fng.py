0567b1ba7e26f7fe0b183d4e50863855d87f432d
evzen <evzen@ifigen.com>
2022-02-06 20:30:37 +0100

    FnG - call sim.fng with explicit parameters

--- runners/run_fng.py
+++ runners/run_fng.py
@@ -7,7 +7,7 @@ import simulators as sim
 start_date = '2021-01-01'
 end_date = '2021-12-31'
 
-data_folder = 'data_crypto'
+data_folder = '../data_crypto'
 
 # BTC/USD
 pair = 'BTC-USD'
@@ -23,7 +23,7 @@ fng_json_path = Path(data_folder, 'FnG_to_2022-01-11.json')
 # init wallet
 wallet = Wallet(init_cash)
 fng_df = utils.load_fng_data(fng_json_path, start_date, end_date)
-sim.fng(fng_df, data_df, wallet, quantum)
+sim.fng(fng_df, data_df, wallet, quantum, buy_threshold=15, buy_threshold_filter=1, sell_threshold=85, sell_threshold_filter=1 )
 
 print(f'\nFinal rate is\t\t{data_df.iloc[-1]["close"]:3.3f}')
 print(f'Profit:\t\t\t\t{wallet.balance_quote(data_df.iloc[-1]["close"]) / init_cash * 100 - 100:3.0f} %')
